e162182d4c5260ca1760e010e3a9634157c46f2a
l3in99 <gkqls99@naver.com>
2024-05-31 14:54:09 +0900

    asd

--- common.js
+++ common.js
@@ -45,13 +45,23 @@ async function initialize() {
 }
 
 function panTo() {
+  const panto = document.getElementById('panTo');
+  // Clear the map container
+  const mapContainer = document.getElementById('map');
+  mapContainer.innerHTML = ''; // Remove all child elements
+  mapContainer.appendChild(panto);
+  
   // 이동할 위도 경도 위치를 생성합니다 
   var moveLatLon = new kakao.maps.LatLng(lat, lng);
+  initMap(lat, lng);
+  clearMarkers();
+  createMarker(lat, lng);
 
   // 지도 중심을 부드럽게 이동시킵니다
   // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
   map.panTo(moveLatLon);
 }
+
 
 
 // 적절한 요소를 보이게 설정하는 함수
