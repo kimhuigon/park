cd5245b7cc05bcf73041e4f478ba89ae1cef73f9
LSG <tkdruddl20@naver.com>
2024-06-03 09:22:12 +0900

    검색버튼 클릭 효과 수정

--- schscript.js
+++ schscript.js
@@ -2,22 +2,17 @@ const buttons = document.querySelectorAll('.ripple')
 
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
-        const x = e.pageX
-        const y = e.pageY
+        const rect = e.target.getBoundingClientRect();
+        const xInside = e.clientX - rect.left;
+        const yInside = e.clientY - rect.top;
 
-        const buttonTop = e.target.offsetTop
-        const buttonLeft = e.target.offsetLeft
+        const circle = document.createElement('span');
+        circle.classList.add('circle');
+        circle.style.top = yInside + 'px';
+        circle.style.left = xInside + 'px';
 
-        const xInside = x - buttonLeft
-        const yInside = y - buttonTop
+        this.appendChild(circle);
 
-        const circle = document.createElement('span')
-        circle.classList.add('circle')
-        circle.style.top = yInside + 'px'
-        circle.style.left = xInside + 'px'
-
-        this.appendChild(circle)
-
-        setTimeout(() => circle.remove(), 500)
-    })
-})
+        setTimeout(() => circle.remove(), 500);
+    });
+});
