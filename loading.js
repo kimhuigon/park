0ce72c61d3f7303e7fe0db4ca7f9b5a6b0fb1c29
SangHee <i3echee@gmail.com>
2024-06-04 10:19:07 +0900

    로딩화면

---
+++ loading.js
@@ -0,0 +1,8 @@
+// 로딩 화면
+document.addEventListener("DOMContentLoaded", function() {
+    // 로딩 프로세스를 시뮬레이트합니다.
+    setTimeout(function() {
+        // Redirect to restpark.html
+        window.location.href = 'restpark.html';
+    }, 2000); // 2초 동안 로딩 화면을 표시합니다.
+});
